0843783742acb802c04a5ed83f78a81258aa16a3
Ridhit Garg <ridhitgarg@ucsb.edu>
2024-07-06 22:06:36 -0700

    Added styling to the app

--- app.py
+++ app.py
@@ -2,105 +2,96 @@ import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 import streamlit as st
-# from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-# from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-def load_openai_api_key():
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+
+def load_api_key():
     dotenv_path = "key.env"
     load_dotenv(dotenv_path)
-    openai_api_key = os.getenv("GOOGLE_API_KEY")
-    if not openai_api_key:
+    api_key = os.getenv("GOOGLE_API_KEY")
+    if not api_key:
         raise ValueError(f"Unable to retrieve GOOGLE_API_KEY from {dotenv_path}")
-    return openai_api_key
+    return api_key
 
 def process_text(text):
-    # Split the text into chunks using Langchain's CharacterTextSplitter
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=50000,
         chunk_overlap=7000,
     )
-    chunks = text_splitter.split_text(text)
 
-    # Convert the chunks of text into embeddings to form a knowledge base
+    chunks = text_splitter.split_text(text)
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     knowledgeBase = FAISS.from_texts(chunks, embeddings)
-
 
     return knowledgeBase
 
 def main():
-    st.title("PDF Summarizer")
-    st.divider()
+    st.markdown('<div class="main">', unsafe_allow_html=True)
+    st.markdown('<h1 class="title">PDF Summarizer</h1>', unsafe_allow_html=True)
+    st.markdown('<div class="divider"></div>', unsafe_allow_html=True)
 
     try:
-        os.environ["GOOGLE_API_KEY"] = load_openai_api_key()
-    except ValueError as e:
-        st.error(str(e))
+        os.environ["GOOGLE_API_KEY"] = load_api_key()
+    except ValueError as err:
+        st.error(str(err))
         return
 
+    st.markdown('<div class="file-uploader">', unsafe_allow_html=True)
     pdf = st.file_uploader('Upload your PDF Document', type='pdf')
+    st.markdown('</div>', unsafe_allow_html=True)
 
     if pdf:
         pdf_reader = PdfReader(pdf)
 
         text = ""
 
+        st.markdown('<span id="input"></span>', unsafe_allow_html=True)
+
         page_start = st.number_input('Page start (1-indexed)', min_value=1, max_value=len(pdf_reader.pages), value=1)
         page_end = st.number_input('Page end (1-indexed)', min_value=page_start, max_value=len(pdf_reader.pages), value=len(pdf_reader.pages))
+        summary_length = st.selectbox('Select Summary Length', ['Brief Summary (1 paragraph)', 'Regular Summary (2-4 paragraphs)', 'Detailed Summary (1 page)'])
 
-        # summary_length = None
-        # if st.button('Brief Summary (3-5 sentences)'):
-        #     summary_length = 'brief'
-        # if st.button('Summary'):
-        #     summary_length = 'summary'
-        # if st.button('Detailed Summary'):
-        #     summary_length = 'detailed'
-        summary_length = st.selectbox('Select Summary Length', ['Brief Summary (3-5 sentences)', 'Summary', 'Detailed Summary'])
+        st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
+        gen_summary_clicked = st.button("Generate Summary!")
+        if gen_summary_clicked:
+            with st.spinner("Generating summary..."):
+                for page_num in range(page_start - 1, page_end):  # Adjust for 0-indexed pages
+                    page = pdf_reader.pages[page_num]
+                    text += page.extract_text()
 
+                knowledgeBase = process_text(text)
 
-        if st.button('Generate Summary'):
+                query_brief = """Give a brief summary of the content of the document in 3 to 5 sentences.
+                Try to capture the main ideas and key points of the document. """
 
-            for page_num in range(page_start - 1, page_end):  # Adjust for 0-indexed pages
-                page = pdf_reader.pages[page_num]
-                text += page.extract_text()
-                # st.write(f"Processed page {page_num + 1}/{len(pdf_reader.pages)}") 
+                query_summary = """Give a summary of the content of the document.
+                Try to capture the main ideas and key points of the document."""
 
-            # st.write(f"Total text length: {len(text)}")
+                query_detailed = """Give a very detailed summary of the content of the document. Write as much as possible.
+                Try to capture the main ideas and key points of the document."""
 
-            knowledgeBase = process_text(text)
+                if summary_length == 'Brief Summary (1 paragraph)':
+                    query = query_brief
+                elif summary_length == 'Regular Summary (2-4 paragraphs)':
+                    query = query_summary
+                else:
+                    query = query_detailed
 
-            query_brief = """Give a brief summary of the content of the document in 3 to 5 sentences.
-            Focus on capturing the main ideas and key points discussed in the document. 
-            Use your own words and ensure clarity and coherence in the summary."""
+                docs = knowledgeBase.similarity_search(query)
+                llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
+                chain = load_qa_chain(llm, chain_type='stuff')
+                response = chain.run(input_documents=docs, question=query)
 
-            query_summary = """Give a summary of the content of the document.
-            Focus on capturing the main ideas and key points discussed in the document. 
-            Use your own words and ensure clarity and coherence in the summary."""
+                st.markdown('<h2>Summary Generated:</h2>', unsafe_allow_html=True)
+                st.markdown(response, unsafe_allow_html=True)
 
-            query_detailed = """Give a very detailed summary of the content of the document. Write as much as possible.
-            Focus on capturing the main ideas and key points discussed in the document. 
-            Use your own words and ensure clarity and coherence in the summary."""
-
-            if summary_length == 'Brief Summary (3-5 sentences)':
-                query = query_brief
-            elif summary_length == 'Summary':
-                query = query_summary
-            else:
-                query = query_detailed
-
-            docs = knowledgeBase.similarity_search(query)
-            llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
-            chain = load_qa_chain(llm, chain_type='stuff')
-            response = chain.run(input_documents=docs, question=query)
-
-            st.subheader('Summary Results:')
-            st.write(response)
+    st.markdown('</div>', unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
